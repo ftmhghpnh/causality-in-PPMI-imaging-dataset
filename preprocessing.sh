fd32e914b505efe40b4dcf7d3bd73d6672095ffe
Beiqin Zeng <52125494+zengbeiqin@users.noreply.github.com>
2021-12-08 17:55:52 -0500

    Update preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -1,1 +1,6 @@
+# skull stripping with ROBEX (Iglesias et al., 2011);
+# If your system is linux-based, download linux-version ROBEX instead of the all-platforms version 
+sh runROBEX.sh [original_file].nii [output_after_skull_stripping].nii
+
+
 
