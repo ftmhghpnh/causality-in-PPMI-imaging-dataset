96aa9ef092c46533b498699a01a810a5129da244
Beiqin Zeng <52125494+zengbeiqin@users.noreply.github.com>
2021-12-08 17:58:41 -0500

    Update preprocessing.sh

--- preprocessing.sh
+++ preprocessing.sh
@@ -2,5 +2,12 @@
 # If your system is linux-based, download linux-version ROBEX instead of the all-platforms version 
 sh runROBEX.sh [original_file].nii [output_after_skull_stripping].nii
 
+# Bias field correction with N4ITK4 (Tustison et al., 2010)
+# pip install SimpleITK before running it
+# download code from https://simpleitk.readthedocs.io/en/master/link_N4BiasFieldCorrection_docs.html as N4BiasFieldCorrection.py 
+python N4BiasFieldCorrection.py [input_name].nii [output_name].nii
+
+
+
 
 
